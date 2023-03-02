66972d60f0255335b2278627d88962aa0d56b897
jaqueline ramos da silva <jaquelineramosit@hotmail.com>
2023-03-01 21:11:58 -0300

    create produts dtos

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,18 +1,35 @@
 import { Body, Controller, Get, Post } from "@nestjs/common/decorators";
 import { CreateProductDTO } from "./createProduct.dto";
+import { ProductEntity } from "./product.entity";
 import { ProductRepository } from "./product.repository";
+import { IncrementId } from "../utils/incrementId"
+import { v4 } from "uuid";
+import { CharacteristicsProductDTO } from "./characteristicsProduct.dto";
 
 @Controller()
 export class ProductController {
 
-    constructor(private productRepository: ProductRepository) {
-        
-    }
+    constructor(private productRepository: ProductRepository, private incrementId: IncrementId) {}
     
     @Post('/product')
-    async createProduct(@Body() dataProduct: CreateProductDTO) {
-        
+    async createProduct(@Body() dataProduct: CreateProductDTO) {        
         try {
+
+            const productEntity = new ProductEntity()
+            const id = await this.incrementId.currentId()
+            const charac = dataProduct.characteristics
+
+        
+
+            
+            productEntity.id = id
+            productEntity.publicId = v4()
+            productEntity.name = dataProduct.name
+            productEntity.value = dataProduct.value
+            productEntity.quantityAvaiable = dataProduct.quantityAvaiable
+            productEntity.description = dataProduct.description
+            productEntity.characteristics = 
+
             return await this.productRepository.save(dataProduct)
         } catch (error) {
             return error
